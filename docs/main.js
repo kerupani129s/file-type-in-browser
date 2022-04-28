87a20cc3dab06096ce757e606835594c803c8af5
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2022-04-28 20:27:50 +0900

    add: 最低限動作する機能追加

---
+++ docs/main.js
@@ -0,0 +1,71 @@
+(() => {
+
+	const convert = (() => {
+
+		const result = document.getElementById('result');
+
+		// 
+		const getFilenameExtension = filename => {
+			const [, ext] = filename.match(/^(?:.+?)(\.[^.]+)?$/);
+			return ext;
+		};
+
+		const escapeHTML = html => html
+			.replaceAll('&', '&amp;')
+			.replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+			.replaceAll('"', '&quot;').replaceAll('\'', '&#39;');
+
+		const convert = files => {
+
+			for (const file of files) {
+
+				const name = file.name;
+				const type = file.type;
+
+				const ext = getFilenameExtension(name);
+
+				const html = '<div>' +
+					escapeHTML(name) + ': ' +
+					(type ? escapeHTML(type) : 'unknown') +
+					(ext ? ' (' + escapeHTML(ext) + ')' : '') +
+					'</div>';
+
+				result.insertAdjacentHTML('afterbegin', html);
+
+			}
+
+		};
+
+		return convert;
+
+	})();
+
+	(() => {
+
+		const inputFileElement = document.getElementById('file');
+
+		const convertOnEvent = files => {
+
+			inputFileElement.disabled = true;
+
+			convert(files);
+
+			inputFileElement.disabled = false;
+
+		};
+
+		// 
+		inputFileElement.addEventListener('click', () => {
+			inputFileElement.value = '';
+		});
+
+		inputFileElement.addEventListener('change', event => {
+			const files = event.target.files;
+			convertOnEvent(files);
+		});
+
+		inputFileElement.disabled = false;
+
+	})();
+
+})();
